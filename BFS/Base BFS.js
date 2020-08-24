9bb12312e358c7921fe1cbe4d5aef63ffa0f457d
Lorenzino65 <lorenzotamer@gmail.com>
2020-08-24 12:59:43 +0200

    basic bfs

---
+++ BFS/Base BFS.js
@@ -0,0 +1,46 @@
+'use strict'
+
+let n=18, //number of nodes
+    g// adjacency list
+
+function bfs(s,e){
+    let prev = solve(s)  //bfs at the start
+
+    return reconstructPath(s,e,prev)  //return the shortest path from s to e
+}
+
+function solve(s){
+    let q = []; //datastructure
+    q.push(s)
+
+    let visited = new Array(n).fill(false)
+    visited[s]=true
+
+    let prev = new Array(n).fill(null) //or map
+    while(!_.isEmpty(q)){
+        node = q.shift()
+        let neighbours = g.get(node)
+
+        neighbours.forEach(next => {
+            if (!visited[next]){
+                q.push(next)
+                visited[next]=true
+                prev[next]=node
+            }    
+        });
+    }
+    return prev
+}
+
+function reconstructPath(s,e,prev){
+
+    let path=[]
+    for(let at =e; at !=null; at = prev[at]){
+        path.push(at)
+    }
+    path.reverse()
+
+    if (path[0]==s) return path
+    return []
+
+}
