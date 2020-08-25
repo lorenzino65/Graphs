a95f9254c0dec1117044c54bdec95071ca2beac3
Lorenzino65 <lorenzotamer@gmail.com>
2020-08-25 20:02:54 +0200

    adjusting files for SSP solution

--- D-AG/TopologicalOrder.mjs
+++ D-AG/TopologicalOrder.mjs
@@ -3,21 +3,23 @@ import dfs from "../DFS/BasicForTopological.mjs"
 export function topsort(algraph){
     let N = algraph.size   
     let seen = new Array(N).fill(false),
-        visitedNodes=[]
+        visitedNodes=[],
+        id = new Map()
+    Array.from(g.keys()).forEach((element,i) => id.set(i,element))
     
     for (let at = 0; at<N;at++){
         if (seen[at]==false){ 
-            visitedNodes =dfs(at,seen,visitedNodes,algraph)
+            visitedNodes =dfs(at,seen,visitedNodes,algraph,id)
         }
     }
     return visitedNodes
 }
 let g =new Map([
-    [0,[1,2]],
-    [1,[3]],
-    [2,[3,5]],
+    [0,[[1],[2]]],
+    [1,[[3]]],
+    [2,[[3],[5]]],
     [3,[]],
-    [4,[2]],
-    [5,[3]]
+    [4,[[2]]],
+    [5,[[3]]]
 ])
 console.log(topsort(g))
--- DFS/BasicForTopological.mjs
+++ DFS/BasicForTopological.mjs
@@ -1,12 +1,12 @@
 'use strict'
 
-export default function dfs (at,visited,order,g){
+export default function dfs (at,visited,order,g,id){
     if (visited[at]) return; //controls if visited or not, returns if alredy seen
     visited[at]=true
-    let neighbours = g.get(at)
+    let neighbours = g.get(id.get(at))
     //goes to check every neighbour
     neighbours.forEach(element => {     
-        return dfs(element,visited,order,g)        
+        return dfs(element[0],visited,order,g,id)        
     });
     order.unshift(at)
     return order
